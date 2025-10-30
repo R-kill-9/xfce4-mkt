898c5bc5313a1fc52d6806fb90f09ab9d4746aa9
Ricard Medina Amado <78033608+R-kill-9@users.noreply.github.com>
2025-10-30 19:57:06 +0100

    Update mkt.sh

--- mkt.sh
+++ mkt.sh
@@ -15,23 +15,23 @@ mkdir -p "$BASE_DIR"/{Evidence,Utils}
 qterminal --working-directory="$BASE_DIR" &
 
 # Wait for the terminal to open
-sleep 1
+sleep 0.5
 
 # Rename the first tab to "Main"
 xdotool key --clearmodifiers alt+shift+s
-sleep 0.1
+sleep 0.05
 xdotool type "Main"
 xdotool key Return
-sleep 0.2
+sleep 0.1
 
 # Open tab for Evidence
 xdotool key --clearmodifiers ctrl+shift+t
-sleep 0.2
+sleep 0.05
 xdotool key --clearmodifiers alt+shift+s
-sleep 0.1
+sleep 0.05
 xdotool type "Evidence"
 xdotool key Return
-sleep 0.2
+sleep 0.05
 # Change directory to Evidence (absolute path)
 xdotool type "cd '$BASE_DIR/Evidence'"
 xdotool key Return
@@ -41,24 +41,26 @@ sleep 0.1
 
 # Open tab for Utils
 xdotool key --clearmodifiers ctrl+shift+t
-sleep 0.2
+sleep 0.05
 xdotool key --clearmodifiers alt+shift+s
-sleep 0.1
+sleep 0.05
 xdotool type "Utils"
 xdotool key Return
 sleep 0.1
 # Change directory to Utils (absolute path)
 xdotool type "cd '$BASE_DIR/Utils'"
 xdotool key Return
-sleep 0.1
+sleep 0.05
 xdotool key --clearmodifiers ctrl+L
 sleep 0.1
 
 # Return focus to Main tab
 xdotool key --clearmodifiers ctrl+Page_Up
-sleep 0.1
+sleep 0.05
 xdotool key --clearmodifiers ctrl+Page_Up
 xdotool type "cd '$BASE_DIR'"
 xdotool key Return
-sleep 0.1
+sleep 0.05
 xdotool key --clearmodifiers ctrl+L
+sleep 0.05
+xdotool key --clearmodifiers ctrl+R
